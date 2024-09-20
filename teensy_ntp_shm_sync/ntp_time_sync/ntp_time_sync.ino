bd5d0e4069a225f6463cd8555ed0707e74ac9035
ANTON <anton.yanovich@hotmail.com>
2024-09-20 12:31:53 -0400

    modified:   catkin_ws/src/mmpug_flir_ros/launch/wildfire/subcanopy-drone-ordv2/wildfire-subcanopy-drone-ordv2-both.launch 	modified:   teensy_ntp_shm_sync/ntp_time_sync/ntp_time_sync.ino

--- teensy_ntp_shm_sync/ntp_time_sync/ntp_time_sync.ino
+++ teensy_ntp_shm_sync/ntp_time_sync/ntp_time_sync.ino
@@ -338,25 +338,28 @@
 
 IntervalTimer timer;        // Create an IntervalTimer object
 const int ppsPin = 1;       // Pin where the PPS signal is connected
-const int outputPin = 2;   // Pin to generate the signal (e.g., an LED or output pin)
+const int outputPin1 = 2;   // Pin to generate the signal (e.g., an LED or output pin)
+const int outputPin2 = 3;   // Pin to generate the signal (e.g., an LED or output pin)
 volatile bool state = LOW;  // To keep track of the pin state (HIGH or LOW)
 
 // This function toggles the output pin
 void togglePin() {
-  digitalWrite(outputPin, state);  // Set the pin to the current state (HIGH or LOW)
+  digitalWrite(outputPin1, state);  // Set the pin to the current state (HIGH or LOW)
+  digitalWrite(outputPin2, state);  // Set the pin to the current state (HIGH or LOW)
   state = !state;                  // Invert the state for the next toggle
 }
 
 // Interrupt function for PPS signal
 void ppsInterrupt() {
   timer.end();                     // Stop the timer if it's running
-  if (state == HIGH) { state = LOW; digitalWrite(outputPin, state);}            // If the state is HIGH, reset it to LOW
+  if (state == HIGH) { state = LOW; digitalWrite(outputPin1, state); digitalWrite(outputPin2, state);}            // If the state is HIGH, reset it to LOW
   
   timer.begin(togglePin, 8333);    // Restart the timer with a 16.67ms period (60Hz)
 }
 
 void setup() {
-  pinMode(outputPin, OUTPUT);      // Set the output pin as an output
+  pinMode(outputPin1, OUTPUT);      // Set the output pin as an output
+  pinMode(outputPin2, OUTPUT);      // Set the output pin as an output
   pinMode(ppsPin, INPUT);          // Set the PPS pin as an input
   attachInterrupt(digitalPinToInterrupt(ppsPin), ppsInterrupt, RISING);  // Trigger on the rising edge of PPS signal
 }
